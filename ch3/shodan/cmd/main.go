07ca26ec3b88c5a0cfef7328bff0c3b6ab7a2805
lineman60 <snyder60>
2020-05-07 20:03:17 -0500

    shodan tool

---
+++ ch3/shodan/cmd/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"black_hat_go/ch3/shodan/shodan"
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		log.Fatalln("Usage: Shodan Searchterm")
+	}
+	apiKey := os.Getenv("SHODAN_API_KEY")
+	s := shodan.New(apiKey)
+	info, err := s.APIInfo()
+	if err != nil {
+		log.Panicln(err)
+	}
+	fmt.Print("Query Credits: %d\nScan Credits:  %d\n\n",
+		info.QueryCredits,
+		info.ScanCredits)
+
+	hostSearch, err := s.HostSearch(os.Args[1])
+	if err != nil {
+		log.Panicln(err)
+	}
+	for _, host := range hostSearch.Matches {
+		fmt.Printf("%18s%8d\n", host.IPString, host.Port)
+	}
+}
